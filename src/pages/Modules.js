34ff4c6f9c8f9d2cb4e06fb27302b5e711695d58
DESKTOP-1M3V8CU\Yagnik <yagnikdevani@gmail.com>
2021-04-17 19:07:33 +0530

    Initial commit

---
+++ src/pages/Modules.js
@@ -0,0 +1,144 @@
+import React,{useState} from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+import CustomizedDialogs from '../components/model/module';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [
+  {
+    moduleId:1,
+    name:"Pharmacy",
+    img:"https://ca.belocum.com/images/modulelogo/icon_pharmacy.png",
+    isModel:false,
+  },
+  {
+    moduleId:2,
+    name:"Nursing Care",
+    img:"https://ca.belocum.com/images/modulelogo/icon_nursing.png",
+    isModel:true,
+  },
+  {
+    moduleId:5,
+    name:"Dental Care",
+    img:"https://ca.belocum.com/images/modulelogo/icon_dental.png",
+    isModel:true,
+  }
+];
+
+export default function Album() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const handleOpen = (isModel) => {
+    setIsOpen(!isOpen);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CustomizedDialogs isDialogOpened={isOpen} handleCloseDialog={() => setIsOpen(false)}></CustomizedDialogs>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Belocum
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              In which field do you work or operate your establishment?
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card.moduleId} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                <CardActionArea onClick={() => (card.isModel) ? handleOpen(card.isModel) : '' } href= { (!card.isModel) ? '#' : '' }>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.img}
+                    title={card.name}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.name}
+                    </Typography>
+                  </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import { Navigate } from 'react-router-dom';
+import DashboardLayout from 'src/components/DashboardLayout';
+import MainLayout from 'src/components/MainLayout';
+import Account from 'src/pages/Account';
+import CustomerList from 'src/pages/CustomerList';
+import Dashboard from 'src/pages/Dashboard';
+import Login from 'src/pages/Login';
+import NotFound from 'src/pages/NotFound';
+import ProductList from 'src/pages/ProductList';
+import Register from 'src/pages/Register';
+import Settings from 'src/pages/Settings';
+import Modules from 'src/pages/Modules';
+
+const routes = [
+  {
+    path: 'app',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'account', element: <Account /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'products', element: <ProductList /> },
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
+      { path: '404', element: <NotFound /> },
+      //{ path: '/', element: <Navigate to="/app/dashboard" /> },
+      { path: '/', element: <Navigate to="modules" /> },
+      { path: '*', element: <Navigate to="/404" /> },
+      { path: 'modules', element: <Modules /> }
+    ]
+  }
+];
+
+export default routes;
